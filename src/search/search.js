682d542da80c24753e9e22271f2c08046c30d69e
moises depena <swe.moisesdepe@gmail.com>
2021-02-17 10:08:26 -0800

    updatedSpinner

--- src/search/search.js
+++ src/search/search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import searchBar from '../search/searchbar.js';
-import Spinner from './Spinner.js';
+import Dropdown from '../search/dropdown.js';
+import Spinner from './spinner.js';
 import request from 'superagent';
 import '../App.css';
 
@@ -9,79 +9,64 @@ export default class SearchPage extends Component {
         pokemon: [],
         sortOrder: '',
         sortBy: '',
-        query: '',
+        filter: '',
         loading: false,
     }
-
     componentDidMount = async () => {
         await this.fetchPokemon();
     }
-
     fetchPokemon = async () => {
         this.setState({ loading: true });
-        const data = await request.get(`https://pokedex-alchemy.xherokuapp.com/api/pokedex?pokemon=${this.state.query}&sort=${this.state.sortBy}&direction=${this.state.sortOrder}`);
-
-        this.setState({
-            pokemon: data.body.results,
-            loading: false,
-        })
+        const myData = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.filter}&direction=${this.state.sortOrder}&sort=${this.state.sortBy}`);
+        this.setState({ pokemon: myData.body.results, loading: false });
     }
-
     handleClick = async () => {
         await this.fetchPokemon();
     }
-
+    handleFilterChange = (e) => {
+        this.setState({ filter: e.target.value });
+    }
+    handleSortOrder = (e) => {
+        this.setState({
+            sortOrder: e.target.value
+        });
+    }
     handleSortBy = (e) => {
         this.setState({
             sortBy: e.target.value
-        })
+        });
     }
-    handleSortOrder = async (e) => {
-        this.setState({
-            sortOrder: e.target.value
-        })
+    render() {
+
+        return (
+            <>
+                <label>
+                    <input onChange={this.handleFilterChange} />
+                </label>
+                <button onClick={this.handleClick}>Search</button>
+                <div>
+                    <Dropdown handleChange={this.handleSortOrder} currentValue={this.state.sortOrder} options={['Ascending', 'Descending']} />
+                    <Dropdown handleChange={this.handleSortBy} currentValue={this.state.sortBy} options={['attack', 'defense', 'type_1', 'pokemon']} />
+                    {
+                        this.state.loading
+                            ? <Spinner />
+                            : this.state.pokemon.map(poke =>
+                                <div key={poke.pokemon}>
+                                    <div>
+                                        <img src={poke.url_image} alt="poke" />
+                                        <p>Name: {poke.pokemon}</p>
+                                        <p>Type: {poke.type_1}</p>
+                                        <p>Attack: {poke.attack}</p>
+                                        <p>Defense: {poke.defense}</p>
+                                    </div>
+
+                                </div>
+
+                            )
+                    }
+
+                </div>
+            </>
+        )
     }
-    handleQuery = async (e) => {
-        this.setState({
-            query: e.target.value
-        })
-    }
-
-
-//     render() {
-//         return (
-//             <>
-//                 <div>
-//                     <searchBar currentValue={this.state.query}
-//                         handleChange={this.handleQuery}
-//                         handleClick={this.handleClick} />
-//             Sort By:
-//                     <sort currentValue={this.state.sortBy} handleChange={this.handleSortBy}
-//                         options={[{ value: 'pokemon', name: 'Pokemon' },
-//                         { value: 'type_1', name: 'Type' },
-//                         { value: 'attack', name: 'Attack' },
-//                         { value: 'defense', name: 'Defense' }]} />
-//                     <sort currentValue={this.state.sortOrder}
-//                         handleChange={this.handleSortOrder}
-//                         options={[{ value: 'Ascend', name: 'Ascending' },
-//                         { value: 'Descend', name: 'Descending' }]} />
-//                 </div>
-//                 <section class="pokemons">
-//                     {this.state.loading
-//                         ? <Spinner />
-//                         : this.state.pokemon.map(poke =>
-//                             <div key={poke.pokemon} class="PokeList">
-//                                 <p><img src={poke.url_image} alt="poke" /></p>
-//                                 <p>Name:{poke.pokemon}</p>
-//                                 <p>Type: {poke.type_1} </p>
-//                                 <p>Attack:{poke.attack}</p>
-//                                 <p>Defense:{poke.defense}</p>
-//                             </div>)
-
-//                     }
-//                 </section>
-//             </>
-//         )
-//     }
-// }
-
+}
--- src/search/spinner.js
+++ src/search/spinner.js
@@ -4,7 +4,7 @@ export default class Spinner extends Component {
     render() {
         return (
             <div>
-                <img width="100" height="100" src="https://tenor.com/view/pokemon-hat-maracas-pokeballs-gif-5420341" />
+                <img width="100" height="100" alt="pokeball" src="https://tenor.com/view/caught-pokemon-pokeball-omg-pokeballs-gif-5284549" />
             </div>
         )
     }
